a235744f8b078b66c340e63170357911e869d807
mike011 <mike_ch_1@hotmail.com>
2017-10-20 15:27:00 -0400

    testing setting password

--- LoginForm/LoginForm/Model/SignupModel.swift
+++ LoginForm/LoginForm/Model/SignupModel.swift
@@ -2,16 +2,26 @@ import Foundation
 
 class SignupModel {
     let userName:String
-    init?(userName: String) {
-        guard SignupModel.validate(userName: userName) else {
+    init?(userName:String, password:String) {
+        guard SignupModel.validate(userName, password) else {
             return nil
         }
         self.userName = userName
+    }
+
+    static func validate(_ userName:String, _ password:String) -> Bool {
+        return validate(userName: userName) && validate(password:password)
     }
 
     static func validate(userName:String) -> Bool {
         let charTest = NSPredicate(format:"SELF MATCHES %@", "[A-Za-z_]+")
         let result = charTest.evaluate(with: userName)
         return result && userName.count>=2 && userName.count<=10
+    }
+
+    static func validate(password:String) -> Bool {
+        let charTest = NSPredicate(format:"SELF MATCHES %@", "[A-Za-z0-9]+")
+        let result = charTest.evaluate(with: password)
+        return result && password.count>=2 && password.count<=10
     }
 }
--- LoginForm/LoginFormTests/SignupModelTests.swift
+++ LoginForm/LoginFormTests/SignupModelTests.swift
@@ -3,30 +3,45 @@ import XCTest
 class SignupModelTests: XCTestCase {
 
     let validUserName = "Valid"
-
+    let validPassword = "Password1"
     func testInit_valid_returnsObject() {
-        let r = SignupModel(userName: validUserName)
+        let r = SignupModel(userName: validUserName, password:validPassword)
         XCTAssertNotNil(r)
         XCTAssertEqual(r!.userName, validUserName)
     }
 
     func testUserName_tooShort_returnsNil() {
-        let r = SignupModel(userName: "")
+        let r = SignupModel(userName: "", password:validPassword)
         XCTAssertNil(r)
     }
 
     func testUserName_tooLong_returnsNil() {
-        let r = SignupModel(userName: "12345678901")
+        let r = SignupModel(userName: "12345678901", password:validPassword)
         XCTAssertNil(r)
     }
 
     func testUserName_underScoreIsAllowed_returnsObject() {
-        let r = SignupModel(userName: "___")
+        let r = SignupModel(userName: "___", password:validPassword)
         XCTAssertNotNil(r)
     }
 
     func testUserName_specialCharactersAreNotAllowed_returnsNil() {
-        let r = SignupModel(userName: "♥♥♥")
+        let r = SignupModel(userName: "♥♥♥", password:validPassword)
+        XCTAssertNil(r)
+    }
+
+    func testPassword_tooShort_returnsNil() {
+        let r = SignupModel(userName: validUserName, password: "")
+        XCTAssertNil(r)
+    }
+
+    func testPassword_tooLong_returnsNil() {
+        let r = SignupModel(userName: validUserName, password: "12345678901")
+        XCTAssertNil(r)
+    }
+
+    func testPassword_withSpace_returnsNil() {
+        let r = SignupModel(userName: validUserName, password: "1 1")
         XCTAssertNil(r)
     }
 }
