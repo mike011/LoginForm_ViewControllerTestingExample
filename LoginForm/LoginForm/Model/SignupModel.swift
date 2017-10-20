f9511cc3fcbe62979581d7f368b72246fbb2f1db
mike011 <mike_ch_1@hotmail.com>
2017-10-20 15:33:56 -0400

    testing of an email address. This would've been a great place to use an email validator object

--- LoginForm/LoginForm/Model/SignupModel.swift
+++ LoginForm/LoginForm/Model/SignupModel.swift
@@ -3,16 +3,19 @@ import Foundation
 class SignupModel {
     let userName:String
     let password:String
-    init?(userName:String, password:String) {
-        guard SignupModel.validate(userName, password) else {
+    let email:String
+
+    init?(userName:String, password:String, email:String) {
+        guard SignupModel.validate(userName, password, email) else {
             return nil
         }
         self.userName = userName
         self.password = password
+        self.email = email
     }
 
-    static func validate(_ userName:String, _ password:String) -> Bool {
-        return validate(userName: userName) && validate(password:password)
+    static func validate(_ userName:String, _ password:String, _ email:String) -> Bool {
+        return validate(userName: userName) && validate(password:password) && validate(email:email)
     }
 
     static func validate(userName:String) -> Bool {
@@ -25,5 +28,12 @@ class SignupModel {
         let charTest = NSPredicate(format:"SELF MATCHES %@", "[A-Za-z0-9]+")
         let result = charTest.evaluate(with: password)
         return result && password.count>=2 && password.count<=10
+    }
+
+    static func validate(email:String) -> Bool {
+        let emailRegEx = "^(?:(?:(?:(?: )*(?:(?:(?:\\t| )*\\r\\n)?(?:\\t| )+))+(?: )*)|(?: )+)?(?:(?:(?:[-A-Za-z0-9!#$%&’*+/=?^_'{|}~]+(?:\\.[-A-Za-z0-9!#$%&’*+/=?^_'{|}~]+)*)|(?:\"(?:(?:(?:(?: )*(?:(?:[!#-Z^-~]|\\[|\\])|(?:\\\\(?:\\t|[ -~]))))+(?: )*)|(?: )+)\"))(?:@)(?:(?:(?:[A-Za-z0-9](?:[-A-Za-z0-9]{0,61}[A-Za-z0-9])?)(?:\\.[A-Za-z0-9](?:[-A-Za-z0-9]{0,61}[A-Za-z0-9])?)*)|(?:\\[(?:(?:(?:(?:(?:[0-9]|(?:[1-9][0-9])|(?:1[0-9][0-9])|(?:2[0-4][0-9])|(?:25[0-5]))\\.){3}(?:[0-9]|(?:[1-9][0-9])|(?:1[0-9][0-9])|(?:2[0-4][0-9])|(?:25[0-5]))))|(?:(?:(?: )*[!-Z^-~])*(?: )*)|(?:[Vv][0-9A-Fa-f]+\\.[-A-Za-z0-9._~!$&'()*+,;=:]+))\\])))(?:(?:(?:(?: )*(?:(?:(?:\\t| )*\\r\\n)?(?:\\t| )+))+(?: )*)|(?: )+)?$"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        let result = emailTest.evaluate(with: email)
+        return result
     }
 }
--- LoginForm/LoginFormTests/SignupModelTests.swift
+++ LoginForm/LoginFormTests/SignupModelTests.swift
@@ -4,45 +4,53 @@ class SignupModelTests: XCTestCase {
 
     let validUserName = "Valid"
     let validPassword = "Password1"
+    let validEmail = "a@b.ca"
+
     func testInit_valid_returnsObject() {
-        let r = SignupModel(userName: validUserName, password:validPassword)
+        let r = SignupModel(userName: validUserName, password:validPassword,email:validEmail)
         XCTAssertNotNil(r)
         XCTAssertEqual(r!.userName, validUserName)
         XCTAssertEqual(r!.password, validPassword)
+        XCTAssertEqual(r!.email, validEmail)
     }
 
     func testUserName_tooShort_returnsNil() {
-        let r = SignupModel(userName: "", password:validPassword)
+        let r = SignupModel(userName: "", password:validPassword,email:validEmail)
         XCTAssertNil(r)
     }
 
     func testUserName_tooLong_returnsNil() {
-        let r = SignupModel(userName: "12345678901", password:validPassword)
+        let r = SignupModel(userName: "12345678901", password:validPassword,email:validEmail)
         XCTAssertNil(r)
     }
 
     func testUserName_underScoreIsAllowed_returnsObject() {
-        let r = SignupModel(userName: "___", password:validPassword)
+        let r = SignupModel(userName: "___", password:validPassword,email:validEmail)
         XCTAssertNotNil(r)
     }
 
     func testUserName_specialCharactersAreNotAllowed_returnsNil() {
-        let r = SignupModel(userName: "♥♥♥", password:validPassword)
+        let r = SignupModel(userName: "♥♥♥", password:validPassword,email:validEmail)
         XCTAssertNil(r)
     }
 
     func testPassword_tooShort_returnsNil() {
-        let r = SignupModel(userName: validUserName, password: "")
+        let r = SignupModel(userName: validUserName, password: "",email:validEmail)
         XCTAssertNil(r)
     }
 
     func testPassword_tooLong_returnsNil() {
-        let r = SignupModel(userName: validUserName, password: "12345678901")
+        let r = SignupModel(userName: validUserName, password: "12345678901",email:validEmail)
         XCTAssertNil(r)
     }
 
     func testPassword_withSpace_returnsNil() {
-        let r = SignupModel(userName: validUserName, password: "1 1")
+        let r = SignupModel(userName: validUserName, password: "1 1",email:validEmail)
+        XCTAssertNil(r)
+    }
+
+    func testEmail_invalid_returnsNil() {
+        let r = SignupModel(userName: validUserName, password: validPassword,email:"invalidEmail")
         XCTAssertNil(r)
     }
 }
