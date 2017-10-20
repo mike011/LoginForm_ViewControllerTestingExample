766ad565a295f41976585d6042086abee37c5669
mike011 <mike_ch_1@hotmail.com>
2017-10-20 14:46:13 -0400

    login model all tested

--- LoginForm/LoginForm/Model/LoginModel.swift
+++ LoginForm/LoginForm/Model/LoginModel.swift
@@ -1,15 +1,25 @@
 import Foundation
 
 class LoginModel {
-    init?(userName:String) {
-        guard validate(userName) else {
+    init?(userName:String, password: String) {
+        guard validate(userName, password) else {
            return nil
         }
     }
 
-    func validate(_ userName:String) -> Bool {
+    func validate(_ userName:String, _ password:String) -> Bool {
+        return validate(userName: userName) && validate(password:password)
+    }
+
+    func validate(userName:String) -> Bool {
         let charTest = NSPredicate(format:"SELF MATCHES %@", "[A-Za-z_]+")
         let result = charTest.evaluate(with: userName)
         return result && userName.count>=2 && userName.count<=10
+    }
+
+    func validate(password:String) -> Bool {
+        let charTest = NSPredicate(format:"SELF MATCHES %@", "[A-Z]+[a-z]+[0-9]+")
+        let result = charTest.evaluate(with: password)
+        return result && password.count>=6 && password.count<=10
     }
 }
--- LoginForm/LoginFormTests/LoginModelTests.swift
+++ LoginForm/LoginFormTests/LoginModelTests.swift
@@ -3,29 +3,55 @@ import XCTest
 class LoginModelTests: XCTestCase {
 
     let validUserName = "Valid"
+    let validPassword = "Password1"
 
     func testInit_valid_returnsObject() {
-        let r = LoginModel(userName: validUserName)
+        let r = LoginModel(userName: validUserName, password: validPassword)
         XCTAssertNotNil(r)
     }
 
     func testUserName_tooShort_returnsNil() {
-        let r = LoginModel(userName: "")
+        let r = LoginModel(userName: "", password: validPassword)
         XCTAssertNil(r)
     }
 
     func testUserName_tooLong_returnsNil() {
-        let r = LoginModel(userName: "12345678901")
+        let r = LoginModel(userName: "12345678901", password: validPassword)
         XCTAssertNil(r)
     }
 
     func testUserName_underScoreIsAllowed_returnsObject() {
-        let r = LoginModel(userName: "___")
+        let r = LoginModel(userName: "___", password: validPassword)
         XCTAssertNotNil(r)
     }
 
     func testUserName_specialCharactersAreNotAllowed_returnsNil() {
-        let r = LoginModel(userName: "♥♥♥")
+        let r = LoginModel(userName: "♥♥♥", password: validPassword)
+        XCTAssertNil(r)
+    }
+
+    func testPassword_tooShort_returnsNil() {
+        let r = LoginModel(userName: validUserName, password: "")
+        XCTAssertNil(r)
+    }
+
+    func testPassword_tooLong_returnsNil() {
+        let r = LoginModel(userName: validUserName, password: "12345678901")
+        XCTAssertNil(r)
+    }
+
+    func testPassword_missingUpperCase_returnsNil() {
+        let r = LoginModel(userName: validUserName, password: "abcdef1")
+        XCTAssertNil(r)
+    }
+
+    func testPassword_missingLowerCase_returnsNil() {
+        let r = LoginModel(userName: validUserName, password: "ABCDEF1")
+        XCTAssertNil(r)
+    }
+
+    func testPassword_missingNumber_returnsNil() {
+        let r = LoginModel(userName: validUserName, password: "ABCDEF")
         XCTAssertNil(r)
     }
 }
